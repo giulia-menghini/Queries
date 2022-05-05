31a08aa0d9eb1b4ea9fb060c9bed5d99163157f4
Giulia Menghini <94474813+giuliamenghini1@users.noreply.github.com>
2022-05-05 09:42:57 -0300

    Update and rename Funding.sql to InfosRav.sql

--- InfosRav.sql
+++ InfosRav.sql
@@ -1,15 +1,18 @@
 SELECT
     LAST_DAY(rav.ContractDate) as referencedate,
-    hub.SalesStructureNameLevel5,
-    SUM(rav.FundingCost) as FundingCost
+    hub.SalesStructureNameLevel5 as Polo,
+    SUM(rav.FundingCost) as FundingCost,
+    SUM(rav.GrossValue) as TpvAntecipado,
+    SUM(rav.Revenue) as ReceitaRav
+
 FROM `ctra-comercial-1554819299431.core_dw_replica.FactRav` rav
 JOIN `ctra-comercial-1554819299431.core_dw_replica.DimAffiliation` aff
     ON rav.ClientAlternateKey = aff.ClientAlternateKey
 JOIN `ctra-comercial-1554819299431.core_dw_replica.ClientListHubs` hub
     ON aff.ClientKey = hub.ClientKey
 WHERE 1=1
-    AND LAST_DAY(rav.ContractDate) >= '2021-01-01'
-    AND LAST_DAY(rav.ContractDate) <= '2021-12-31'
+    AND LAST_DAY(rav.ContractDate) >= '2022-04-01'
+    AND LAST_DAY(rav.ContractDate) <= '2022-04-30'
     AND hub.SalesStructureNameLevel2 = 'FRANQUIA'
 GROUP BY
     LAST_DAY(rav.ContractDate),
